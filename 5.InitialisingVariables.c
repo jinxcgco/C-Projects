07fa892a2f2ce7949d357377c4f703a47e69385f
HermadesCGCO <hermadescgco@disroot.org>
2021-04-11 19:55:31 +0000

    Added another program

---
+++ 5.InitialisingVariables.c
@@ -0,0 +1,29 @@
+/* Copyright (C) 2021 Jinx.
+   This file is part of CProjects.
+   Created by Jinx CGCO <HermadesCGCO@disroot.org>, 2021.
+
+   CProjects is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License V3 as published by the
+   Free Software Foundation.
+
+   CProjects is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRAMTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License V3
+   for more details.
+
+   You should have received a copy of the GNU General Public License V3
+   along with CProjects; if not, see
+   <https://www.gnu.org/licenses/>. */
+
+#include <stdio.h>
+
+int
+main ()
+{
+  int integer_value = 10;
+  float float_value = 10.0f;
+  char char_value = 'A';
+  double double_value;
+
+  return 0;
+}
--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@ gcc 1.Basic.c -o 1.Basic.o
 gcc 2.Comments.c -o 2.Comments.o
 gcc 3.Output.c -o 3.Output.o
 gcc 4.Input.c -o 4.Input.o
+gcc 5.InitialisingVariables.c -o 5.InitialisingVariables.o
