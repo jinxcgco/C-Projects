ce74c4d4c04d427b1a0259fa6130da2c2adf6dcb
HermadesCGCO <hermadescgco@disroot.org>
2021-04-11 16:50:06 +0000

    Added two new programs

---
+++ 3.Output.c
@@ -0,0 +1,29 @@
+/* Copyright (C) 2021 Jinx.
+   This file is part of CProjects.
+   Created by Jinx CGCO <HermadesCGCO@disroot.org>, 2021.
+
+   CProjects is free software; you can redistribute it and/or modify it
+   under the terms of the GNU General Public License V3 as published by the
+   Free Software Foundation.
+
+   CProjects is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRAMTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License V3
+   for more details.
+
+   You should have received a copy of the GNU General Public License V3
+   along with CProjects; if not, see
+   <https://www.gnu.org/licenses/>. */
+
+#include <stdio.h>
+
+int
+main ()
+{
+  int a = 100;
+  float b = 10.0;
+
+  printf("Hello, I am printing this text to the screen :3\n");
+  printf("The value of a is %d and the value of b is %f\n", a, b);
+  return 0;
+}
--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 gcc 1.Basic.c -o 1.Basic.o
 gcc 2.Comments.c -o 2.Comments.o
+gcc 3.Output.c -o 3.Output.o
+gcc 4.Input.c -o 4.Input.o
